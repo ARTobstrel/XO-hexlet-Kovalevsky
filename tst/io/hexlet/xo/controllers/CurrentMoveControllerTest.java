654d8e8154b659b6ff8a1b4eadee395e4186a78f
artem <artemfap@yandex.ru>
2018-02-26 23:50:00 +0300

    CurrentMoveControllerTest init

---
+++ tst/io/hexlet/xo/controllers/CurrentMoveControllerTest.java
@@ -0,0 +1,50 @@
+package io.hexlet.xo.controllers;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class CurrentMoveControllerTest {
+
+    @Test
+    public void getCurrentMoveIsX() throws InvalidPointException {
+        final CurrentMoveController currentMoveController = new CurrentMoveController();
+        final Field field = new Field(3);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 0), Figure.O);
+        assertEquals(Figure.X, currentMoveController.currentMove(field));
+        }
+
+    @Test
+    public void getCurrentMoveIsO() throws InvalidPointException {
+        final CurrentMoveController currentMoveController = new CurrentMoveController();
+        final Field field = new Field(3);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 0), Figure.O);
+        field.setFigure(new Point(0, 0), Figure.X);
+        assertEquals(Figure.O, currentMoveController.currentMove(field));
+        }
+
+    @Test
+    public void getCurrentMoveIsNull() throws InvalidPointException {
+        final CurrentMoveController currentMoveController = new CurrentMoveController();
+        final Field field = new Field(3);
+
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 0), Figure.O);
+        field.setFigure(new Point(0, 0), Figure.X);
+        field.setFigure(new Point(2, 2), Figure.O);
+        field.setFigure(new Point(2, 1), Figure.X);
+        field.setFigure(new Point(0, 1), Figure.O);
+        field.setFigure(new Point(1, 0), Figure.X);
+        field.setFigure(new Point(1, 2), Figure.O);
+        field.setFigure(new Point(0, 2), Figure.X);
+        assertNull(currentMoveController.currentMove(field));
+    }
+
+}
