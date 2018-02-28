71d03e10c952b1545fc196bf23b729c753ab30ea
artem <artemfap@yandex.ru>
2018-02-28 23:21:20 +0300

    finish ConsoleView

--- src/io/hexlet/xo/view/ConsoleView.java
+++ src/io/hexlet/xo/view/ConsoleView.java
@@ -1,14 +1,24 @@
 package io.hexlet.xo.view;
 
+import io.hexlet.xo.controllers.CurrentMoveController;
+import io.hexlet.xo.controllers.MoveController;
+import io.hexlet.xo.controllers.WinnerController;
 import io.hexlet.xo.model.Field;
 import io.hexlet.xo.model.Figure;
 import io.hexlet.xo.model.Game;
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
 import io.hexlet.xo.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 import java.util.Scanner;
 
 public class ConsoleView {
+
+    private final CurrentMoveController currentMoveController = new CurrentMoveController();
+
+    private final WinnerController winnerController = new WinnerController();
+
+    private final MoveController moveController = new MoveController();
 
     public void show (final Game game) {
         System.out.printf("Game name: %s\n", game.getGameName());
@@ -20,8 +30,31 @@ public class ConsoleView {
         }
     }
 
-    public void move(final Game game) {
+    public boolean move(final Game game) {
+        final Field field = game.getField();
+        final Figure currentFigure = currentMoveController.currentMove(field);
+        if (currentFigure == null){
+            final Figure winner = winnerController.getWinner(field);
+            if (winner == null) {
+                System.out.println("No winner and no moves left!");
+                return false;
+            } else {
+                System.out.printf("Winner is: %s\n", winner);
+                return false;
+            }
+        }
+        System.out.printf("Please, enter move point for: %s\n", currentFigure);
+        final Point point = askPoint();
+        try {
+            moveController.applyFigure(field, point, currentFigure);
+        } catch (InvalidPointException | AlreadyOccupiedException e) {
+            System.out.println("Point is invalid!");
+        }
+        return true;
+    }
 
+    private Point askPoint() {
+        return new Point(askCordinate("X")-1, askCordinate("Y")-1);
     }
 
     private int askCordinate(final String coordinateName) {
